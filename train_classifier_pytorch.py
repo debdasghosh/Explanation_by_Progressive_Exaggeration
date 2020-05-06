dcc56f8f19faf2974ee529c215d3a268b33f44a3
Debdas Ghosh <89debdas@gmail.com>
2020-05-06 16:29:45 -0400

    Add files via upload

---
+++ train_classifier_pytorch.py
@@ -0,0 +1,186 @@
+import numpy as np
+import pandas as pd
+import sys 
+import os
+import pdb
+import yaml
+from utils import read_data_file, load_images_and_labels
+import argparse
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+warnings.filterwarnings("ignore")
+
+
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.utils import data as udata
+from torch.utils.data import DataLoader
+import math
+import densenet
+
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+    '--config', '-c', default='configs/celebA_DenseNet_Classifier.yaml'
+)   
+    args = parser.parse_args()
+    # ============= Load config =============
+    config_path = args.config
+    config = yaml.load(open(config_path))
+    print(config)
+    # ============= Experiment Folder=============
+    output_dir = os.path.join(config['log_dir'], config['name'])
+    try: os.makedirs(output_dir)
+    except: pass
+    try: os.makedirs(os.path.join(output_dir, 'logs'))
+    except: pass
+    # ============= Experiment Parameters =============
+    BATCH_SIZE = config['batch_size']
+    EPOCHS = config['epochs']
+    channels = config['num_channel']
+    input_size = 32 #config['input_size'] 
+    N_CLASSES = config['num_classes'] 
+    ckpt_dir_continue = config['ckpt_dir_continue']    
+    if ckpt_dir_continue == '':
+        continue_train = False
+    else:
+        continue_train = True
+    # ============= Data =============
+    try:
+        categories, file_names_dict = read_data_file(config['image_label_dict'])
+    except:
+        print("Problem in reading input data file : ", config['image_label_dict'])
+        sys.exit()
+    data_train = np.load(config['train'])
+    data_test = np.load(config['test'])
+    print("The classification categories are: ")
+    print(categories)
+    print('The size of the training set: ', data_train.shape[0])
+    print('The size of the testing set: ', data_test.shape[0])
+    fp = open(os.path.join(output_dir, 'setting.txt'), 'w')
+    fp.write('config_file:'+str(config_path)+'\n')
+    fp.close()
+
+
+    torch.manual_seed(1)
+
+    '''
+    ns = data_train[0:int(BATCH_SIZE)*2]
+    xs, ys = load_images_and_labels(ns, config['image_dir'],N_CLASSES, file_names_dict, input_size, channels, do_center_crop=True)
+    tensor_x = torch.Tensor(xs) 
+    tensor_y = torch.Tensor(ys)
+    trainDataset = udata.TensorDataset(tensor_x,tensor_y) # create your datset
+    trainLoader = udata.DataLoader(trainDataset, batch_size=BATCH_SIZE, num_workers=2, shuffle=True) # create your dataloader
+    '''
+
+    ns = data_test[0:int(BATCH_SIZE)*2]
+    xs, ys = load_images_and_labels(ns, config['image_dir'],N_CLASSES, file_names_dict, input_size, channels, do_center_crop=True)
+    tensor_x = torch.Tensor(xs) 
+    tensor_y = torch.Tensor(ys)
+    testDataset = udata.TensorDataset(tensor_x,tensor_y) # create your datset
+    testLoader = udata.DataLoader(testDataset, batch_size=BATCH_SIZE, num_workers=1, pin_memory=True, shuffle=False) # create your dataloader
+
+    trainLoader = testLoader
+    
+    '''
+    normMean = [0.49139968, 0.48215827, 0.44653124]
+    normStd = [0.24703233, 0.24348505, 0.26158768]
+    normTransform = transforms.Normalize(normMean, normStd)
+
+    trainTransform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normTransform
+    ])
+    testTransform = transforms.Compose([
+        transforms.ToTensor(),
+        normTransform
+    ])
+    kwargs = {'num_workers': 1, 'pin_memory': True} 
+    trainLoader = DataLoader(
+        dset.CIFAR10(root='cifar', train=True, download=True,
+                     transform=trainTransform),
+        batch_size=BATCH_SIZE, shuffle=True, **kwargs)
+    testLoader = DataLoader(
+        dset.CIFAR10(root='cifar', train=False, download=True,
+                     transform=testTransform),
+        batch_size=BATCH_SIZE, shuffle=False, **kwargs)
+    
+    N_CLASSES = 10
+    '''
+    #for batch_idx, (data, target) in enumerate(trainLoader):
+        #data, target = data.cuda(), target.cuda()
+        #print(data)
+
+
+    net = densenet.DenseNet(growthRate=12, depth=100, reduction=0.5, bottleneck=True, nClasses=N_CLASSES)
+    #net = densenet3.DenseNet3(40, N_CLASSES, 12, reduction=1.0, bottleneck=False, dropRate=0.0)
+    net = net.cuda()
+    optimizer = optim.Adam(net.parameters(), weight_decay=1e-4)
+
+    for epoch in range(1, EPOCHS + 1):
+            train( epoch, net, trainLoader, optimizer)
+            test( epoch, net, testLoader, optimizer)
+            torch.save(net, os.path.join('./', 'latest.pth'))
+
+
+
+def train( epoch, net, trainLoader, optimizer ):
+    net.train()
+    nProcessed = 0
+    nTrain = len(trainLoader.dataset)
+    for batch_idx, (data, target) in enumerate(trainLoader):
+        data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = net(data)
+        loss = F.binary_cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        nProcessed += len(data)
+        pred = output.data.max(1)[1] 
+        incorrect = pred.ne(target.data).cpu().sum()
+        err = 100.*incorrect/len(data)
+        partialEpoch = epoch + batch_idx / len(trainLoader) - 1
+        print('Train Epoch: {:.2f} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tError: {:.6f}'.format(
+            partialEpoch, nProcessed, nTrain, 100. * batch_idx / len(trainLoader),
+            loss.data, err)) #loss.data[0]
+
+
+def test( epoch, net, testLoader, optimizer):
+    net.eval()
+    test_loss = 0
+    incorrect = 0
+    for data, target in testLoader:
+        data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = net(data)
+        test_loss += F.binary_cross_entropy(output, target).data[0]
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        incorrect += pred.ne(target.data).cpu().sum()
+
+    test_loss = test_loss
+    test_loss /= len(testLoader) # loss function already averages over batch size
+    nTotal = len(testLoader.dataset)
+    err = 100.*incorrect/nTotal
+    print('\nTest set: Average loss: {:.4f}, Error: {}/{} ({:.0f}%)\n'.format(
+        test_loss, incorrect, nTotal, err))
+
+
+
+
+if __name__ == "__main__":
+    run()
---
+++ utils.py
@@ -0,0 +1,107 @@
+import os
+import pickle
+import numpy as np
+from tqdm import tqdm
+import scipy.misc as scm
+from PIL import Image
+import pdb
+import os
+from glob import glob
+from collections import namedtuple
+
+def crop_center(img,cropx,cropy):
+    #y,x,z = img.shape
+    #startx = x//2-(cropx//2)
+    #starty = y//2-(cropy//2)    
+    #return img[starty:starty+cropy,startx:startx+cropx,:]
+
+    width, height = img.size 
+    left = width//2-(cropx//2)
+    top = height//2-(cropy//2)
+    right = width//2+(cropx//2)
+    bottom = height//2+(cropy//2) 
+    return img.crop((left, top, right, bottom))
+
+def read_data_file(file_path, image_dir=''):
+    attr_list = {}   
+    path = file_path
+    file = open(path,'r')    
+    n = file.readline()
+    n = int(n.split('\n')[0]) #  Number of images    
+    attr_line = file.readline()
+    attr_names = attr_line.split('\n')[0].split() # attribute name
+    for line in file:
+        row = line.split('\n')[0].split()
+        img_name = os.path.join(image_dir, row.pop(0))
+        try:
+            row = [float(val) for val in row]
+        except:
+            print(line)
+            img_name = img_name + ' '+row[0]
+            row.pop(0)
+            row = [float(val) for val in row]            
+#    img = img[..., ::-1] # bgr to rgb
+        attr_list[img_name] = row       
+    file.close()
+    return attr_names, attr_list
+
+def load_images_and_labels(imgs_names, image_dir, n_class, attr_list, input_size=128, num_channel=3, do_center_crop=False):
+    imgs = np.zeros((imgs_names.shape[0], num_channel, input_size, input_size), dtype=np.float32)
+    labels = np.zeros((imgs_names.shape[0], n_class), dtype=np.float32)
+
+    for i, img_name in tqdm(enumerate(imgs_names)):
+        #img = scm.imread(os.path.join(image_dir, img_name))
+        img = Image.open(os.path.join(image_dir, img_name))
+        if do_center_crop and input_size == 128:
+            img = crop_center(img, 150,150)
+        #img = scm.imresize(img, [input_size, input_size, num_channel])
+        img = img.resize((input_size, input_size))
+        
+        #img = np.reshape(img, [input_size,input_size,num_channel])
+        img = np.reshape(img, [num_channel,input_size,input_size])
+        img = img / 255.0 
+        img = img - 0.5
+        img = img * 2.0
+        
+        imgs[i] = img
+        try:
+            labels[i] = attr_list[img_name]
+        except:
+            print(img_name)
+            pdb.set_trace()
+    labels[np.where(labels==-1)] = 0
+    return imgs, labels
+
+def inverse_image(img): 
+    img = (img + 1.) * 127.5
+    img[img > 255] = 255.
+    img[img < 0] = 0.
+    return img.astype(np.uint8)
+    
+def make3d(img, num_channel, image_size, row, col):
+    # img.shape = [row*col, h, w, c]
+    # final: [row*h, col*w, c]
+    if num_channel > 1:
+        img = np.reshape(img, [col,row,image_size,image_size,num_channel]) # [col, row, h, w, c]
+    else:
+        img = np.reshape(img, [col,row,image_size,image_size]) # [col, row, h, w]
+    img = unstack(img, axis=0) # col * [row, h, w, c]
+    img = np.concatenate(img, axis=2) # [row, h, col*w, c]
+    img = unstack(img, axis=0) # row * [h, col*w, c]
+    img = np.concatenate(img, axis=0) # [row*h, col*w, c]
+    return img
+
+
+def unstack(img, axis):
+    d =img.shape[axis]
+    arr = [np.squeeze(a,axis=axis) for a in np.split(img, d, axis=axis)]
+    return arr
+    
+def save_images(realA, realB, fakeB, cycA, sample_file, image_size=128, num_channel = 3):
+    img = np.concatenate((realA[:5,:,:,:],fakeB[:5,:,:,:], cycA[:5,:,:,:], realB[:5,:,:,:],
+                          realA[5:,:,:,:],fakeB[5:,:,:,:], cycA[5:,:,:,:], realB[5:,:,:,:]), axis=0)
+                          
+    img = make3d(img, num_channel=num_channel, image_size=image_size, row=5, col=8)                      
+    img = inverse_image(img)
+    #scm.imsave(sample_file, img)
+    img.save(sample_file, "JPEG")
